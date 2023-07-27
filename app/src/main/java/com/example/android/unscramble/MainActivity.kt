d342683170899aa7aedfe6e58dcc89615726d71a
ermolenkoA <kypalo2012@gmail.com>
2023-07-27 17:38:39 +0300

    Added sample code

--- app/src/main/java/com/example/android/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -14,5 +14,18 @@
  * limitations under the License.
  */
 
-include ':app'
-rootProject.name = "Unscramble"
+package com.example.android.unscramble
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Creates an Activity that hosts the Game fragment in the app
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
